0466aaffb612549918888a1b60314d30e445f0b3
Darwin <salinash2000@gmail.com>
2019-08-05 21:02:52 -0600

    rutas para los catalogos necesarios para los impuestos

--- routes/web.php
+++ routes/web.php
@@ -2,7 +2,23 @@
 Route::get('/', function () {
     return view('welcome');
 });
-
 Auth::routes();
-
 Route::get('/home', 'HomeController@index')->name('home');
+
+Route::apiResource('ambitos-impuestos', 'AmbitoImpuestoController', [
+    'parameters' => [
+        'ambitos-impuestos' => 'model'
+    ]
+]);
+
+Route::apiResource('calculos-impuestos', 'CalculoImpuestoController', [
+    'parameters' => [
+        'calculos-impuestos' => 'model'
+    ]
+]);
+
+Route::apiResource('impuestos', 'ImpuestoController', [
+    'parameters' => [
+        'impuestos' => 'model'
+    ]
+]);
